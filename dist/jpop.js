d5bbac5a8b96758f26dbe8aa8b94961b13307204
Patrick Shampine <pshampine@gunnjerkens.com>
2015-08-14 15:25:44 -0700

    Minor changes to boilerplate at end of license, incorrect license in package, version

--- dist/jpop.js
+++ dist/jpop.js
@@ -1,9 +1,19 @@
 /**
-Version: 1.0.1
-Build: Wed, 12 Aug 2015 00:57:46 GMT
+Version: 1.0.2
+Build: Fri, 14 Aug 2015 22:25:01 GMT
 
--- License --
-GPLv3 -- https://github.com/jetstash/jpop/blob/master/LICENSE
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://github.com/jetstash/jpop/blob/master/LICENSE.
 
 Copyright (c) 2015 Jetstash LLC
 **/
